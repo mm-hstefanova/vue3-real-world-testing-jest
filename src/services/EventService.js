084f07c05d549871b7286e9f2c82bbf086556bf1
hstefanova <hristiyana.stefanova@mentormate.com>
2023-01-12 14:31:50 +0200

    updates on endpoints

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL:
+    'https://my-json-server.typicode.com/mm-hstefanova/vue3-real-world-testing-jest',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
